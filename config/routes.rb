9b5055676e3be65dd33eb78df1f698a248db82e6
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-10 15:59:28 -0700

    Added event model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "events#index"
 
   resources :events
+  resources :users, only: [:show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_10_224949) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_225559) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_10_224949) do
     t.datetime "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "events", "users"
 end
