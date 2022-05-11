d2075a8055b990a55e50ffbfb09e119b43d280ce
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-11 09:10:33 -0700

    Added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :events
 
+  before_validation :set_name, on: :create
+
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  private
+
+  def set_name
+    self.name = "User ##{rand(1000)}" if self.name.blank?
+  end
 end
