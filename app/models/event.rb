f76f202017eb88c3d39f8e5eb3cc404f2209dfb0
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-12 10:43:40 -0700

    Added comment model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :events
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 35}
 end
