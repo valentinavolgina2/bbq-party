931c4ddb36e87abb88ceefd123fdff7a67fee421
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-12 11:52:38 -0700

    Added subsription model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events
+  has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 35}
 end
