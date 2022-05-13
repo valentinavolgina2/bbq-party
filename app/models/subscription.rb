ad1f626570d61699cc60f74e74e13c4c1c317132
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-12 21:27:10 -0700

    Restricted anonymous use registered emails

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,6 +6,7 @@ class Subscription < ApplicationRecord
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  validate :cannot_user_other_user_email
 
   def user_name
     if user.present?
@@ -20,6 +21,14 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def cannot_user_other_user_email
+    if user_email && User.exists?(email: user_email)
+      errors.add(:user_email, I18n.t('controllers.subscriptions.email_is_taken'))
     end
   end
 end
