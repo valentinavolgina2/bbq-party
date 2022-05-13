1f55398c8accd72ac293d47fc381404cd8094c1f
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-13 16:37:32 -0700

    Added with options to subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,13 +2,18 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  with_options if: :user.present? do
+    validates :user, uniqueness: {scope: :event_id}, if: -> {user.present?}
+  end
 
-  validate :cannot_subscribe_on_my_event
+  with_options unless: :user.present? do
+    validates :user_name, presence: true, unless: -> {user.present?}
+    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> {user.present?}
+    validates :user_email, uniqueness: {scope: :event_id}, unless: -> {user.present?}
+  end
+
   validate :cannot_use_other_user_email
+  validate :cannot_subscribe_on_my_event
 
   def user_name
     if user.present?
