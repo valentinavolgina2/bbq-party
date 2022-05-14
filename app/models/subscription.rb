68eeee6206be691358306607637fe7213c2a74ca
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-14 09:19:10 -0700

    Fixed with options syntax

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,17 +2,17 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  with_options if: :user.present? do
-    validates :user, uniqueness: {scope: :event_id}, if: -> {user.present?}
+  with_options if: -> {user.present?} do
+    validates :user, uniqueness: {scope: :event_id}
   end
 
-  with_options unless: :user.present? do
-    validates :user_name, presence: true, unless: -> {user.present?}
-    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> {user.present?}
-    validates :user_email, uniqueness: {scope: :event_id}, unless: -> {user.present?}
+  with_options unless: -> {user.present?} do
+    validates :user_name, presence: true
+    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+    validates :user_email, uniqueness: {scope: :event_id}
+    validate :cannot_use_other_user_email
   end
 
-  validate :cannot_use_other_user_email
   validate :cannot_subscribe_on_my_event
 
   def user_name
@@ -40,7 +40,7 @@ class Subscription < ApplicationRecord
   end
 
   def cannot_use_other_user_email
-    if !user.present? && User.exists?(email: user_email)
+    if User.exists?(email: user_email)
       errors.add(:user_email, :email_is_taken)
     end
   end
