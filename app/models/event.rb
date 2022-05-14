822a26f38c84ad1d6e0375fc25d6e25512ff9bb2
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-14 11:48:48 -0700

    Added photos to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
+  has_many :photos, dependent: :destroy
 
   has_many :subscribers, through: :subscriptions, source: :user
 
---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,21 @@
+class PhotoUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_to_fit: [100, 100]
+  end
+
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+end
