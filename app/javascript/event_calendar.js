acd0845a479b87637c906cee5301ead5cafcd005
valentinavolgina2 <valentina.volgina2@gmail.com>
2022-05-21 16:40:46 -0700

    Removed comment

--- app/javascript/event_calendar.js
+++ app/javascript/event_calendar.js
@@ -3,10 +3,9 @@ import AirDatepicker from 'air-datepicker';
 document.addEventListener("turbo:load", function() {
   const datetimeInput = document.getElementById('event-date')
   if (!datetimeInput) return
-  
+
   let selectedDate = []
   if (datetimeInput.value) {
-    console.log(datetimeInput.value)
     selectedDate = [new Date(datetimeInput.value.replace(' UTC',''))]
   }
 
